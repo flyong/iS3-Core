9b10544aef1679018e4972595ef88ecf1ea7b5c7
lzkqcc <353616653@qq.com>
2018-04-27 00:37:27 +0800

    add iS3 Project
    Create iS3 Project

---
+++ iS3.Core/Serialization/DGObjectLoader.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iS3.Core.Serialization
+{
+    #region Copyright Notice
+    //************************  Notice  **********************************
+    //** This file is part of iS3
+    //**
+    //** Copyright (c) 2015 Tongji University iS3 Team. All rights reserved.
+    //**
+    //** This library is free software; you can redistribute it and/or
+    //** modify it under the terms of the GNU Lesser General Public
+    //** License as published by the Free Software Foundation; either
+    //** version 3 of the License, or (at your option) any later version.
+    //**
+    //** This library is distributed in the hope that it will be useful,
+    //** but WITHOUT ANY WARRANTY; without even the implied warranty of
+    //** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    //** Lesser General Public License for more details.
+    //**
+    //** In addition, as a special exception,  that plugins developed for iS3,
+    //** are allowed to remain closed sourced and can be distributed under any license .
+    //** These rights are included in the file LGPL_EXCEPTION.txt in this package.
+    //**
+    //**************************************************************************
+    #endregion
+    // This is a default object loader for DGObject.
+    // You are encouraged to write your object loader for each 
+    // new class that derived from DGObject
+    //
+    public class DGObjectLoader
+    {
+        protected DbDataLoader _dbLoader;
+
+
+        public DGObjectLoader()
+        {
+            _dbLoader = new DbDataLoader();
+        }
+        public bool Load(DGObjects objs)
+        {
+            DGObjectsDefinition def = objs.definition;
+            if (def == null)
+                return false;
+
+            bool success = _dbLoader.ReadDGObjects(objs,
+                def.TableNameSQL, def.DefNamesSQL, def.OrderSQL, def.ConditionSQL);
+
+            return success;
+        }
+    }
+}
